4f217a1437c9223016ce19934c90228ae9c34745
Rebekah <114865790+Refactor-Rebekah@users.noreply.github.com>
2023-07-24 16:30:56 -0500

    program upload
    program upload

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace PoisonousApp
+{
+    class Program
+    {
+        private static readonly HttpClient client = new HttpClient();
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Menu: ");
+            Console.WriteLine("1. Search by Species Name: ");
+            Console.WriteLine("2. Search by Species ID: ");
+            Console.WriteLine("3. End: ");
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            if (choice == 1)
+            {
+                Console.WriteLine("Please enter the name of the animal you would like to search for: ");
+                string animal = Console.ReadLine();
+                await SearchBySpecies(animal);
+            }
+            else if (choice == 2)
+            {
+                Console.WriteLine("Please enter the ID fo the animal you would like to search for: ");
+                int TaxonID = Convert.ToInt32(Console.ReadLine());
+                await SpeciesByTaxon(TaxonID);
+            }
+            else
+            {
+                return;
+            }
+            
+        }
+        private static async Task SpeciesByTaxon(int TaxonID)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync("https://apps.des.qld.gov.au/species/?op=getspeciesbyid&taxonid=" + TaxonID);
+            var speciesresult = await JsonSerializer.DeserializeAsync<GetSpeciesByID>(await streamTask);
+
+            var msg = await streamTask;
+            Console.Write(msg);
+        }
+        private static async Task SearchBySpecies(string animal)
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            //client.DefaultRequestHeaders.Accept.Add(
+            //new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            //client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var streamTask = client.GetStreamAsync($"https://apps.des.qld.gov.au/species/?op=speciessearch&kingdom=animals&species={animal}");
+            var speciesresult = await JsonSerializer.DeserializeAsync<SpeciesSearch>(await streamTask);
+
+            var msg = await streamTask;
+            Console.Write(msg);
+        }
+    }
+}
---
+++ Species.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PoisonousApp
+{
+    public class Species
+    {
+        public int TaxonID { get; set; }
+        public string ScientificName { get; set; }
+        public string AcceptedCommonName { get; set; }
+        public string KingdomCommonName { get; set; }
+        public string FamilyCommonName { get; set; }
+        public string PestStatus { get; set; }
+       
+        public class SpeciesImage
+        {
+            public string URL { get; set; }
+        }
+    }
+}
